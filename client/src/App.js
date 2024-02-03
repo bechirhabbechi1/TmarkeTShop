9eeeb955f530870494394d69fb19002a120201a3
bechirhabbechi1 <bechirhabbechi1@gmail.com>
2024-02-03 20:56:06 +0100

    br

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Tmarket</a>
-      </header>
-      <main>list product</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <a href="/">Tmarket</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
